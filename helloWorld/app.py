fe033a8e2add7e4dda2c47c92d165c0a88f47bd4
Austin Beaufort <tinyhatcoding@gmail.com>
2019-06-09 09:49:00 -0400

    python course

---
+++ helloWorld/app.py
@@ -0,0 +1,27 @@
+#PRINT
+
+# print('austin beaufort, hello world!')
+# print('o----')
+# print(' ||||')
+# print('*' * 10)
+
+# print (' * ' * 2)
+# print (' * ' * 3)
+# print (' * ' * 2)
+
+
+# VARIABLES
+
+# price = 10
+# price = 20
+# rating = 4.9
+# name = 'austin'
+# is_published = True
+# print(price, name, is_published)
+
+# variable exercise
+# name = 'John Smith'
+# age = 20
+# new_patient = True
+
+# print(name, age, new_patient)
